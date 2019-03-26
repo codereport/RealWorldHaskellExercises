cf50f5f4582517e6a321766a2a86e3f1d932df97
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2019-03-26 01:57:27 -0700

    Update Ch04.hs

--- Ch04.hs
+++ Ch04.hs
@@ -38,6 +38,38 @@ asInt_fold s@(sign:num)
     | otherwise   = stoi s
         where stoi s = foldl (\a b -> a * 10 + digitToInt b) 0 s
 
+-- Exercise 3
+-- Technically fails a couple case
+--  1. Nondigit first character that isn't -
+--  2. Values of String == to maxBount length but still greater
+asInt_fold2 :: String -> Int
+asInt_fold2 s@(sign:num)
+    | null s      = 0 -- this fails?
+    | s == "-"    = 0
+    | nondigit    = error "Not all characters are digits"
+    | overflow    = error "String value is to large"
+    | sign == '-' = -1 * stoi num
+    | otherwise   = stoi s
+        where stoi s = foldl (\a b -> a * 10 + digitToInt b) 0 s
+              nondigit = any (not . isDigit) num
+              overflow = length s > length (show (maxBound :: Int))
+
+-- Exercise 4 ... skipped
+
+-- Exercise 5 & 6
+concat' :: [[a]] -> [a]
+concat' = foldr (++) []
+
+-- Exercsie 7
+takewhile' :: (a -> Bool) -> [a] -> [a]
+takewhile' _ [] = []
+takewhile' p (x:xs)
+    | p x = x : takewhile' p xs
+    | otherwise = []
+
+takewhile'' :: (a -> Bool) -> [a] -> [a]
+takewhile'' p = foldr (\b a -> if p b then b : a else []) []
+
 main :: IO ()
 main = do
     print $ safeHead ([] :: [Int])
@@ -49,4 +81,14 @@ main = do
     print $ (asInt_fold "1234") * 10
     print $ asInt_fold "101"
     print $ asInt_fold "-31337"
-    print $ asInt_fold "1798" 
+    print $ asInt_fold "1798"
+    -- print $ asInt_fold2 "" -- can't get this to work
+    print $ asInt_fold2 "-"
+    --print $ asInt_fold2 "2.7"
+    --print $ asInt_fold2 "314159265358979323846"
+    -- print $ asInt_fold2 "?123" -- this fails
+    print $ concat' [[1],[2],[3]]
+    print $ takewhile' (<5) [1..10]
+    print $ takewhile' (<5) []
+    print $ takewhile'' (<5) [1..10]
+    print $ takewhile'' (<5) []
